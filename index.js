a964e5527dff1e83dca6499e1dd51c2c0245f2d1
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-04-27 22:21:26 -0300

    exercicio

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ alert('Divisão');
 
 let a = prompt('digite o numerador:');
 let b = prompt('Digite o denominador:');
- alert(`${a} dividido pro ${b} é igual:${(a/b)}`);
+ alert(`${a} dividido por ${b} é igual:${(a/b)}`);
